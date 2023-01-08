5876071a63d425629e2f156684a7c43f2d3e4f44
aseemsavio <aseemsavio3@gmail.com>
2023-01-08 12:47:38 +0530

    loom

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,55 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.1"
-	id("io.spring.dependency-management") version "1.1.0"
-	id("org.graalvm.buildtools.native") version "0.9.18"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+    id("org.springframework.boot") version "3.0.1"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.graalvm.buildtools.native") version "0.9.18"
+    kotlin("jvm") version "1.8.0"
+    kotlin("plugin.spring") version "1.8.0"
 }
 
 group = "com.asavio"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_19
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-graphql")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework:spring-webflux")
-	testImplementation("org.springframework.graphql:spring-graphql-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-graphql")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework:spring-webflux")
+    testImplementation("org.springframework.graphql:spring-graphql-test")
+}
+
+val preview = "--enable-preview"
+
+tasks.withType<org.springframework.boot.gradle.tasks.run.BootRun> {
+    jvmArgs = mutableListOf(preview)
+}
+
+tasks.withType<JavaExec> {
+    jvmArgs = mutableListOf(preview)
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add(preview)
+    options.compilerArgs.add("-Xlint:preview")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "19"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- src/main/kotlin/com/asavio/loomgraphql/LoomgraphqlApplication.kt
+++ src/main/kotlin/com/asavio/loomgraphql/LoomgraphqlApplication.kt
@@ -1,11 +1,52 @@
 package com.asavio.loomgraphql
 
+import org.apache.coyote.ProtocolHandler
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.core.task.AsyncTaskExecutor
+import org.springframework.core.task.support.TaskExecutorAdapter
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
+import java.util.concurrent.Executors
+
 
 @SpringBootApplication
 class LoomgraphqlApplication
 
 fun main(args: Array<String>) {
-	runApplication<LoomgraphqlApplication>(*args)
+    runApplication<LoomgraphqlApplication>(*args)
 }
+
+@Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+fun asyncTaskExecutor(): AsyncTaskExecutor? {
+    return TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor())
+}
+
+@Bean
+fun protocolHandlerVirtualThreadExecutorCustomizer(): TomcatProtocolHandlerCustomizer<*>? {
+    return TomcatProtocolHandlerCustomizer { protocolHandler: ProtocolHandler ->
+        protocolHandler.executor = Executors.newVirtualThreadPerTaskExecutor()
+    }
+}
+
+@Controller
+class PeopleController {
+
+    @QueryMapping
+    fun people(): List<Person> {
+        println("Is Virtual Thread: ${Thread.currentThread().isVirtual} | ${Thread.currentThread()}")
+        return listOf(
+            Person("jdgahjdagdahjdg", "Jon", 25),
+            Person("ddgahjdagdahjdg", "Snow", 26),
+            Person("gdgahjdagdahjdg", "Arya", 23),
+            Person("qdgahjdagdahjdg", "Stark", 21),
+        )
+    }
+
+}
+
+data class Person(val id: String, val name: String, val age: Int)
+
