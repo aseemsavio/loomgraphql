27c24510171aacb8a56d5730b78a3fcd3d29eae3
aseemsavio <aseemsavio3@gmail.com>
2023-01-08 11:06:16 +0530

    first commit

---
+++ src/main/kotlin/com/asavio/loomgraphql/LoomgraphqlApplication.kt
@@ -0,0 +1,11 @@
+package com.asavio.loomgraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoomgraphqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoomgraphqlApplication>(*args)
+}
---
+++ src/test/kotlin/com/asavio/loomgraphql/LoomgraphqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.asavio.loomgraphql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoomgraphqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
